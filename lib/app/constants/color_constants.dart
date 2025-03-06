e4cdaa94a750b527db1b5efdb2b35efa56ced551
Ashmita124 <126133726+Ashmita124@users.noreply.github.com>
2025-03-06 18:09:35 +0545

    hive table

--- lib/app/constants/color_constants.dart
+++ lib/app/constants/color_constants.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-
+// 
 const KPrimaryColor = Color(0xFF6F35A5);
 const KPrimaryLightColor = Color(0xFFF1E6FF);
 const kGreyColor = Color.fromARGB(147, 0, 0, 0);
